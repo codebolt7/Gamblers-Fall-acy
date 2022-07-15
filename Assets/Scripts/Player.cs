65becc8c189be5b46bb30c3943863e348960de5b
Codebolt <44513207+codebolt7@users.noreply.github.com>
2022-07-15 14:33:51 -0700

    Added Character Movement Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,21 +7,34 @@ public class Player : MonoBehaviour
     Controls controls;
     Rigidbody2D rb;
 
+    [Header("Object Assignment")]
+    [SerializeField] GameObject attack;
+    [SerializeField] SpriteRenderer spriteTop;
+    [SerializeField] SpriteRenderer spriteBottom;
+
     [Header("Movement")]
     [SerializeField] float speed = 5f;
 
     [Header("Attack")]
-    [SerializeField] GameObject attack;
     [SerializeField] float attackDistance = 1;
 
-    [Header("Animations")]
-    [SerializeField] SpriteRenderer spriteTop;
-    [SerializeField] SpriteRenderer spriteBottom;
+    [Header("Object Assignment")]
+    [SerializeField] float frameDelay;
+    int idleFrameIndex = 0;
+    [SerializeField] Sprite[] animIdleTop;
+    [SerializeField] Sprite[] animIdleBottom;
+    float walkTimer;
+    int walkFrameIndex = -1;
+    [SerializeField] Sprite[] animWalkTop;
+    [SerializeField] Sprite[] animWalkBottom;
+    [SerializeField] Sprite[] animAttackTop;
+
+    bool attacking;
 
     void Awake()
     {
         controls = new Controls();
-        controls.Player.Attack.performed += _ => Attack();
+        controls.Player.Attack.performed += _ => StartCoroutine(Attack());
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -41,17 +54,63 @@ public class Player : MonoBehaviour
     {
         Vector2 input = controls.Player.Move.ReadValue<Vector2>();
         rb.velocity = input * speed;
-        Debug.Log(input);
-        spriteBottom.flipX = input.x > 0;
+
+        BaseAnimate(input);
     }
 
-    private void Attack()
+    private void BaseAnimate(Vector2 input)
     {
+        walkFrameIndex = input.magnitude > 0.1f ? Mathf.Max(walkFrameIndex, 0) : -1;
+
+        // Walk
+        if (walkFrameIndex >= 0)
+        {
+            spriteBottom.sprite = animWalkBottom[walkFrameIndex];
+            spriteBottom.flipX = input.x > 0;
+            if (!attacking)
+            {
+                spriteTop.sprite = animWalkTop[walkFrameIndex];
+                spriteTop.flipX = input.x > 0;
+            }
+
+            walkTimer += Time.deltaTime;
+            if (walkTimer > frameDelay)
+            {
+                walkFrameIndex = (walkFrameIndex + 1) % animWalkBottom.Length;
+                walkTimer = 0;
+            }
+        }
+        // Idle
+        else
+        {
+            spriteBottom.sprite = animIdleBottom[idleFrameIndex];
+            spriteBottom.flipX = input.x > 0;
+            if (!attacking)
+            {
+                spriteTop.sprite = animIdleTop[idleFrameIndex];
+                spriteTop.flipX = input.x > 0;
+            }
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        if (attacking) yield break;
+        attacking = true;
+
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(controls.Player.MousePosition.ReadValue<Vector2>());
         Vector2 relativePos = (worldPos - (Vector2)transform.position).normalized;
 
         attack.transform.position = relativePos * attackDistance + (Vector2)transform.position;
         spriteTop.flipX = relativePos.x > 0;
+
+        foreach (Sprite frame in animAttackTop)
+        {
+            spriteTop.sprite = frame;
+            yield return new WaitForSeconds(frameDelay);
+        }
+
+        attacking = false;
     }
 
     void OnEnable()
